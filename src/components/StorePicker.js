7ca7930084fc13660986461f3d05e44680337e22
Gustavo Abdelnour <gurabdelnour@gmail.com>
2019-12-16 14:18:29 -0500

    adding routing form to take user to right store name

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,18 +1,23 @@
 import React from "react";
-import { getFunName } from '../helpers'
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
-  myInput = React.createRef()
+  myInput = React.createRef();
+
+  goToStore = event => {
+    // 1. Stop the form from submitting
+    event.preventDefault();
+    // 2. get the text from that input
+    const storeName = (this.myInput.current.value);
+    // 3. Change the page to /store/whatever-they-entered
+    this.props.history.push(`/store/${storeName}`)
+  };
   
-  goToStore = (event) => {
-    event.preventDefault()
-    console.log(this)
-  }
   render() {
     return (
       <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input 
+        <input
           type="text"
           ref={this.myInput}
           required
