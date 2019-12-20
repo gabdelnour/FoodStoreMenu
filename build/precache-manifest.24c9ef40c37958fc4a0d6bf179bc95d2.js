0a998f3aa07c0b52d86289fe3e6d77446ca9de90
Gustavo Abdelnour <gurabdelnour@gmail.com>
2019-12-20 18:49:49 -0500

    added scrips to package.json and enabled user log in for managing inventory

---
+++ build/precache-manifest.24c9ef40c37958fc4a0d6bf179bc95d2.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52d95ddff08ea2c486503372f7d68804",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2645f804b11ec43bfd35",
+    "url": "/static/css/main.1d05f41f.chunk.css"
+  },
+  {
+    "revision": "76e10c5521dff2585ff9",
+    "url": "/static/js/2.24bd3e8c.chunk.js"
+  },
+  {
+    "revision": "b34c9566e700109d0e1c54b13d6307a3",
+    "url": "/static/js/2.24bd3e8c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2645f804b11ec43bfd35",
+    "url": "/static/js/main.a0b50d64.chunk.js"
+  },
+  {
+    "revision": "262af5c9f91400622deb",
+    "url": "/static/js/runtime-main.e3b52510.js"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "7bbffda6bcacac8360f0ee00bb03dc04",
+    "url": "/static/media/blanch_caps_inline-webfont.7bbffda6.svg"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "cf798e30eda73d84b25c4ae233fe5e21",
+    "url": "/static/media/haymaker-webfont.cf798e30.svg"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/static/media/haymaker-webfont.e9535f20.ttf"
+  }
+]);
